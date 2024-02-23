f07af89c173f86653fc6dc3d3856cc4d587c5a9c
greeshma0906 <147246445+greeshma0906@users.noreply.github.com>
2024-02-24 01:00:04 +0530

    Update bubblesort.asm

--- bubblesort.asm
+++ bubblesort.asm
@@ -1,31 +1,31 @@
 .data
-array: .word 14,2,0,9,10,31,28,11,5,60
+array: .word 14 2 0 9 10 31 28 11 5 60
 .text
 .globl main
 main:
-li x0,0             #arr[0]
-li x1,0             #i = 0
-li x2,0             #j = 0
-li x3,10             #n = 10
-li x4,10             #n-i for inner loop
+li x0,0            
+li x1,0           
+li x2,0             
+li x3,10            
+li x4,10             
 li x9,0
 addi x3,x3,-1
 outerloop:
 li x2,0
-addi x4,x4,-1       #decreasing size for inner_loop
-addi x7,x0,0        #resetting addr itr j
+addi x4,x4,-1      
+addi x7,x0,0        
 innerloop:
-lw x5,0(x7)         #arr[j]
-addi x7,x7,1        #addr itr j += 4
-lw x6,0(x7)         #arr[j+1]
-addi x2,x2,1        #j++
-slt x8,x5,x6        #set x8 = 1 if x5 < x6
+lw x5,0(x7)        
+addi x7,x7,1        
+lw x6,0(x7)         
+addi x2,x2,1        
+slt x8,x5,x6        
 bne x8,x9,leave
 swap:
 sw x5,0(x7)
 sw x6,-1(x7)
 lw x6,0(x7)
 leave:
-bne x2,x4,innerloop     #j != n-i
-addi x1,x1,1            #i++
-bne x1,x3,outerloop     #i != n
+bne x2,x4,innerloop     
+addi x1,x1,1           
+bne x1,x3,outerloop     
