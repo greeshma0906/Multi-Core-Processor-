0bb061c133a76b31306173f175a822cbf978c4ac
Snehitha1312 <147246727+Snehitha1312@users.noreply.github.com>
2024-02-25 00:07:28 +0530

    Update bubblesort.asm

--- bubblesort.asm
+++ bubblesort.asm
@@ -1,31 +1,35 @@
-.data
-array: .word 14 2 0 9 10 31 28 11 5 60
+.data 
+arr: .word 41 44 59 52 98 3 8 19 57 25 
 .text
-.globl main
 main:
-li x0 0            
-li x1 0           
-li x2 0             
-li x3 10            
-li x4 10             
-li x9 0
-addi x3 x3 -1
-outerloop:
 li x2 0
-addi x4 x4 -1      
-addi x7 x0 0        
-innerloop:
-lw x5 0(x7)        
-addi x7 x7 1        
-lw x6 0(x7)         
-addi x2 x2 1        
-slt x8 x5 x6        
-bne x8 x9 leave
+li x3 10
+addi x4 x3 -1
+li x5 0
+li x11 0
+loop:
+lw x8 0(x5)
+lw x9 1(x5)
+bgt x8 x9 swap
+addi x5 x5 1
+sub x11 x3 x2
+addi x11 x11 -1
+blt x5 x11 loop
+addi x2 x2 1
+addi x12 x3 -1
+blt x2 x12 loop2
+exit
 swap:
-sw x5 0(x7)
-sw x6 -1(x7)
-lw x6 0(x7)
-leave:
-bne x2 x4 innerloop     
-addi x1 x1 1           
-bne x1 x3 outerloop     
+sw x8 1(x5)
+sw x9 0(x5)
+addi x5 x5 1
+sub x11 x3 x2
+addi x11 x11 -1
+blt x5 x11 loop
+addi x2 x2 1
+addi x12 x3 -1
+blt x2 x12 loop2
+exit
+loop2:
+li x5 0
+j loop
