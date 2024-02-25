01ed3ba92949618bee8ad2d64d8c00baf5da1ca0
greeshma0906 <147246445+greeshma0906@users.noreply.github.com>
2024-02-25 23:25:55 +0530

    Update selectionsort.asm

--- selectionsort.asm
+++ selectionsort.asm
@@ -15,14 +15,14 @@ addi x20 x19 1
 addi x21 x19 0
 addi x4 x2 1
 loop2:
-bge x4 x12 vi
+bge x4 x12 loope
 lw x8 0(x20)
 lw x9 0(x21)
 blt x8 x9 min_ind
 addi x20 x20 1
 addi x4 x4 1
 j loop2
-vi:    
+loope:    
 bne x3 x2 swap 
 addi x19 x19 1
 addi x2 x2 1
